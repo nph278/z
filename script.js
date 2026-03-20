ce9830b104fd09034db46f6927ee6bc138e5e4e8
nph278 <carllegrone@protonmail.com>
2026-03-20 08:47:05 -0400

    fix alert max

--- script.js
+++ script.js
@@ -48,7 +48,7 @@ play_scream = () => {
 make_notification = (s) => {
     uuid++;
     document.querySelector("#notifications").insertAdjacentHTML("beforeend", "<div class='notification' id='not"+uuid+"' onclick=\"document.querySelector('#not"+uuid+"').outerHTML=''\">" + s + " (CLICK2REMOVE)</div>");
-    if (((15 * document.querySelectorAll(".notification").length) / window.innerHeight) > .23) {
+    while (((15 * document.querySelectorAll(".notification").length) / window.innerHeight) > .23) {
         document.querySelectorAll(".notification")[0].outerHTML = "";
     }
 }
