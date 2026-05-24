b6adef645549e5f573f0d80d6165d869eaeac652
nph278 <carllegrone@protonmail.com>
2026-05-23 20:01:28 -0400

    game update

--- emrpg.js
+++ emrpg.js
@@ -1,6 +1,5 @@
 // Hi Reid
 
-// Link from game page
 // Trailer
 // More text effets: shaking, ripple
 // Repeatedly use that number "[n] ... Xthings" >495?
@@ -11,6 +10,8 @@
 // Dialogue text effect
 // Better opening line
 // Balancing for the ending reactions
+// 700 scene
+// Better 5k bg
 
 "use strict";
 
@@ -66,6 +67,8 @@ const makeSound = (filename) => {
 
 const soundPhone = makeSound("rpgphone.mp3");
 const soundCrystal = makeSound("rpgcrystal.wav");
+const soundOwen = makeSound("owenlacy.mp3");
+soundOwen.loop = true;
 
 class Scene {
     constructor(img, sound) {
@@ -85,14 +88,18 @@ class Scene {
     }
 }
 
-const scene_intro = new Scene("rpgbg1.gif", "rpgbg2.mp3");
+const scene_intro = new Scene("rpgbg1.gif", "crickets.mp3");
 const scene_main = new Scene("rpgbg4.gif", "rpgbg2.mp3");
+const scene_closet = new Scene("rpgcloset.gif", "zdshr_closet.mp3");
+const scene_tape = new Scene("rpgtape.gif", "tape.mp3");
 const scene_fourk = new Scene("rpgbg3.gif", "rpgbg1.mp3");
+const scene_fivek = new Scene("rpg5k.gif", "rpg5k.mp3");
 const scene_underground = new Scene("rpgbg2.gif", "rpgbg2.mp3");
 const scene_trailers = new Scene("rpgbg2.gif", "rpgbg2.mp3");
 const scene_tunnel = new Scene("rpgtunnel.gif", "banger.mp3");
 const scene_musical = new Scene("rpgmusical.gif", "rpgmusical.mp3");
 const scene_900 = new Scene("rpg900.gif", "rpg900.mp3");
+const scene_pool = new Scene("rpgpool.gif", "rpgpool.mp3");
 
 class TextStyle {
     constructor() {
@@ -477,6 +484,8 @@ class Game {
             if (endIDs.includes(id)) {
                 if (this.lastScene !== null) {
                     this.lastScene.unapply();
+                    soundOwen.load();
+                    soundOwen.play();
                 }
                 soundCrystal.load();
                 soundCrystal.play();
@@ -521,7 +530,30 @@ class Game {
                 this.enterScene(scene_musical);
             } else if (id === "nineh1") {
                 this.enterScene(scene_900);
+            } else if (id === "guardeddoor") {
+                this.enterScene(scene_main);
+            } else if (id === "avcloset") {
+                this.enterScene(scene_closet);
+            } else if (id === "tape1") {
+                this.enterScene(scene_tape);
+            } else if (id === "tapeend") {
+                this.enterScene(scene_closet);
+            } else if (id === "mediaside") {
+                this.enterScene(scene_main);
+            } else if (id === "fivekback") {
+                this.enterScene(scene_main);
+            } else if (id === "fivekfront") {
+                this.enterScene(scene_main);
+            } else if (id === "fivekstairs1a") {
+                this.enterScene(scene_fivek);
+            } else if (id === "fivekstairs2a") {
+                this.enterScene(scene_fivek);
+            } else if (id === "pool3") {
+                this.enterScene(scene_pool);
+            } else if (id === "fivekelevatorc") {
+                this.enterScene(scene_fivek);
             }
+
 
             if (id === "tunnel4") {
                 this.parkerFight = true;
@@ -729,6 +761,7 @@ class Game {
             }
         } else if (a.type === "reset") {
             this.reset();
+            soundOwen.pause();
         } else if (a.type === "back") {
             soundPhone.load();
             soundPhone.play();
@@ -5387,10 +5420,6 @@ addEventListener('load', async (event) => {
     (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) ismobile = true;})(navigator.userAgent||navigator.vendor||window.opera);
 
 
-    await Promise.all(
-        images.concat(sounds).map((a) => new Promise((resolve) => a.complete ? resolve() : a.addEventListener("load", resolve)))
-    );
-
     const canvas = document.querySelector("canvas");
     canvas.width = width;
     canvas.height = height;
@@ -5403,8 +5432,18 @@ addEventListener('load', async (event) => {
 
     ctx.fillStyle = "white";
     ctx.font = "100px Courier Prime, courier, monospace";
-    ctx.fillText("CLICK2BEGIN (Audio On)", 0, 100);
+    ctx.fillText("LOADING (Turn Audio On)", 0, 100);
 
+    await Promise.all(
+        images.concat(sounds).map((a) => new Promise((resolve) => a.complete ? resolve() : a.addEventListener("load", resolve)))
+    );
+
+    ctx.fillStyle = "black";
+    ctx.fillRect(0, 0, width, height);
+
+    ctx.fillStyle = "white";
+    ctx.font = "100px Courier Prime, courier, monospace";
+    ctx.fillText("CLICK2BEGIN (Audio On)", 0, 100);
 
     await new Promise(resolve => canvas.addEventListener("click", resolve));
 
